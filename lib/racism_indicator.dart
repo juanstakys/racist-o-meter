862b18a530a03c76af14a5e0db069a1265d236cc
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-08-09 13:50:46 -0300

    Widgets modularisation

--- lib/racism_indicator.dart
+++ lib/racism_indicator.dart
@@ -1,9 +1,13 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:info_popup/info_popup.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:avatar_glow/avatar_glow.dart';
+
+import 'widgets/racism_indicator_widgets.dart' show MicButton, Indicator;
 
 class RacismIndicator extends StatefulWidget {
   const RacismIndicator({super.key});
@@ -34,7 +38,8 @@ class RacismIndicatorState extends State<RacismIndicator> {
   }
 
   void _startListening() async {
-    await _speechToText.listen(onResult: _onSpeechResult);
+    await _speechToText.listen(
+        partialResults: false, onResult: _onSpeechResult);
     setState(() {});
   }
 
@@ -59,104 +64,117 @@ class RacismIndicatorState extends State<RacismIndicator> {
           ),
         ),
         child: Center(
-          child: FutureBuilder(
-            future: racismDetection(_lastWords),
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.none:
-                  return const Center(
-                    child: Text(
-                      "Say something",
-                      style: TextStyle(
-                        fontSize: 36,
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 255, 255, 255),
-                      ),
-                    ),
-                  );
-                case ConnectionState.waiting:
-                  return const Center(
-                    child: Text(
-                      "Waiting for GPT response",
-                      style: TextStyle(
-                        fontSize: 36,
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 255, 255, 255),
-                      ),
-                    ),
-                  );
-                case ConnectionState.active:
-                  return const Center(
-                    child: Text("This text should not display. WTH?"),
-                  );
-                case ConnectionState.done:
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const SizedBox(
-                          width:
-                              40), // Sizedbox for the text to stay centered (balances with InfoPopupWidget) TO-DO: Check wether it is a good practice or not, find alternative to keep the text centered with the icon on the right
-                      Text(
-                        (_isItRacist ? "RACIST" : "NOT RACIST"),
-                        style: const TextStyle(
-                          fontSize: 36,
-                          fontStyle: FontStyle.italic,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(255, 255, 255, 255),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: InfoPopupWidget(
-                          contentTitle: _explanation,
-                          child: Icon(Icons.info),
-                        ),
-                      )
-                    ],
-                  );
-              } // switch statement
-            },
-          ),
+          child: _lastWords == ''
+              ? const Text(
+                  "Press the button to speak",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                  ),
+                )
+              : FutureBuilder(
+                  future: racismDetection(_lastWords),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.none:
+                        return const Center(
+                          child: Text(
+                            "Say something",
+                            style: TextStyle(
+                              fontSize: 36,
+                              fontStyle: FontStyle.italic,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            ),
+                          ),
+                        );
+                      case ConnectionState.waiting:
+                        return const Center(
+                          child: Text(
+                            "Waiting for GPT response",
+                            style: TextStyle(
+                              fontSize: 36,
+                              fontStyle: FontStyle.italic,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            ),
+                          ),
+                        );
+                      case ConnectionState.active:
+                        return const Center(
+                          child: Text("This text should not display. WTH?"),
+                        );
+                      case ConnectionState.done:
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            const SizedBox(
+                                width:
+                                    40), // Sizedbox for the text to stay centered (balances with InfoPopupWidget) TO-DO: Check wether it is a good practice or not, find alternative to keep the text centered with the icon on the right
+                            Text(
+                              (_isItRacist ? "RACIST" : "NOT RACIST"),
+                              style: const TextStyle(
+                                fontSize: 36,
+                                fontStyle: FontStyle.italic,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 255, 255, 255),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: InfoPopupWidget(
+                                contentTitle: _explanation,
+                                child: Icon(Icons.info),
+                              ),
+                            )
+                          ],
+                        );
+                    } // switch statement
+                  },
+                ),
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _speechToText.isNotListening ? _startListening() : _stopListening();
-        },
-        shape: const CircleBorder(),
-        tooltip: 'Press the button and speak',
-        child: const Icon(Icons.mic),
-      ),
+      floatingActionButton: MicButton(isListening: true)
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     _speechToText.isNotListening ? _startListening() : _stopListening();
+      //   },
+      //   shape: const CircleBorder(),
+      //   tooltip: 'Press the button to speak',
+      //   child: const Icon(Icons.mic),
+      // ),
     );
   }
 
   // On speech result
   void _onSpeechResult(SpeechRecognitionResult speechResult) async {
-    if (speechResult.finalResult) {
-      _lastWords = speechResult.recognizedWords;
-      // Call backend API to get whether the phrase was racist or not
-      analysisResults = await racismDetection(_lastWords);
-      setState(() {
-        _isItRacist = analysisResults?["isItRacist"];
-        _explanation = analysisResults?["explanation"];
-      });
-    }
+    _lastWords = speechResult.recognizedWords;
+    // Call backend API to get whether the phrase was racist or not
+    analysisResults = await racismDetection(_lastWords);
+    setState(() {
+      _isItRacist = analysisResults?["isItRacist"];
+      _explanation = analysisResults?["explanation"];
+    });
   }
 
   // API Call
   Future<Map> racismDetection(String statement) async {
     // TO-DO: Set server url from a variable
-    var url = Uri.http('192.168.0.13:8080', 'deteccion');
-    var response = await http.post(
-      url,
-      headers: {"Content-Type": "application/json"},
-      body: json.encode({'statement': statement}),
-    );
-    Map<String, dynamic> data = jsonDecode(response.body);
-    print(response.body);
-    return data;
+    // var url = Uri.http('192.168.0.13:8080', 'deteccion');
+    // var response = await http.post(
+    //   url,
+    //   headers: {"Content-Type": "application/json"},
+    //   body: json.encode({'statement': statement}),
+    // );
+    // Map<String, dynamic> data = jsonDecode(response.body);
+    // print(response.body);
+    return Future.delayed(const Duration(seconds: 2), () {
+      Map<String, dynamic> data = jsonDecode(
+          '{"isItRacist": ${!_isItRacist}, "explanation": "HAHA, hello!"}');
+      return data;
+    });
   }
 }
---
+++ lib/widgets/racism_indicator_widgets.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:avatar_glow/avatar_glow.dart';
+
+class Indicator extends StatelessWidget {
+  final String text = '';
+
+  const Indicator({super.key, required text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 36,
+        fontStyle: FontStyle.italic,
+        fontWeight: FontWeight.bold,
+        color: Color.fromARGB(255, 255, 255, 255),
+      ),
+    );
+  }
+}
+
+class MicButton extends StatelessWidget {
+  bool isListening = true;
+  
+
+  MicButton({super.key, required this.isListening});
+
+  @override
+  Widget build(BuildContext context) {
+    return AvatarGlow(
+      endRadius: isListening ? 40.0 : 0,
+      child: FloatingActionButton(
+        onPressed: () {},
+        shape: const CircleBorder(),
+        tooltip: 'Press the button to speak',
+        child: const Icon(Icons.mic),
+      ),
+    );
+  }
+}
